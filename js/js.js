14710d0a224783766770b8e98c96b2ab9fb63c9c
Benjamin Marks <bjmarks13@yahoo.com>
2022-07-01 14:20:27 -0500

    commit-for ben

--- js/js.js
+++ js/js.js
@@ -1,11 +1,34 @@
 "use strict";
 const coffeeTemplate = document.querySelector("[data-coffee-template]"); // hidden html template to auto-fill with js
-const coffeeContainer = document.querySelector("[data-coffee-contaier]"); // empty div that shows the serched coffees
+let coffeeContainer = document.querySelector("#coffeeContainer"); // empty div that shows the serched coffees
 const searchInput = document.querySelector("[data-search]"); // serach data coffee input bar
 const searchButton = document.getElementById("search-submit") // search coffee submit button
 const searchCoffeeRoast = document.getElementById("search-coffee-roast"); // roast selection for coffee search
 const searchCoffeeName = document.getElementById("search-coffee-name"); // name search for coffee search
 let searchCoffees = []; // empty array to push coffees into
+
+let coffees = `[
+    {"id": 1, "name": "Light City", "roast": "light"},
+    {"id": 2, "name": "Half City", "roast": "light"},
+    {"id": 3, "name": "Cinnamon", "roast": "light"},
+    {"id": 4, "name": "City", "roast": "medium"},
+    {"id": 5, "name": "American", "roast": "medium"},
+    {"id": 6, "name": "Breakfast", "roast": "medium"},
+    {"id": 7, "name": "High", "roast": "dark"},
+    {"id": 8, "name": "Continental", "roast": "dark"},
+    {"id": 9, "name": "New Orleans", "roast": "dark"},
+    {"id": 10, "name": "European", "roast": "dark"},
+    {"id": 11, "name": "Espresso", "roast": "dark"},
+    {"id": 12, "name": "Viennese", "roast": "dark"},
+    {"id": 13, "name": "Italian", "roast": "dark"},
+    {"id": 14, "name": "French", "roast": "dark"}
+]`;
+let coffeesList = JSON.parse(coffees);
+coffeesList.forEach(coffeetype, index => {
+    coffeeContainer.innerHTML = 0;    
+});
+coffeeContainer.innerHTML = `<h1>${coffeesList[0].name} </h1> `//<p>${coffeesList.roast} </p>`;
+console.log(coffeesList);
     searchInput.addEventListener("input", (e) => {
         const value = e.target.value;
         console.log(value.toLowerCase());
@@ -15,6 +38,7 @@ let searchCoffees = []; // empty array to push coffees into
     searchButton.addEventListener("click", function (event) {
         event.preventDefault();
         let searchCoffee = {
+            // id: Date.now(),
             name: searchCoffeeName.value, 
             roast: searchCoffeeRoast.value
         }
@@ -31,10 +55,10 @@ let searchCoffees = []; // empty array to push coffees into
         //     console.log(coffee);
         //     return {name: coffee.name, roast: coffee.roast, element: card};
         // })
-    function addCoffeeToList() {
+   // function addCoffeeToList() {
         let addCoffeeSubmit = document.getElementById("add-submit");
         addCoffeeSubmit.addEventListener("click", function(event){
             event.preventDefault();
             console.warn(`added to coffee list storage`);
         });
-    }
+    //}
